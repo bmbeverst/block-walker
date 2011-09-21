a17e29a98fb75d9ca0800a3a06b95757c91e1fca
Cyb3r.Kn19ht@gmail.com <Cyb3r.Kn19ht@gmail.com@d6edabdd-0a54-e1d9-4704-f514ea89214e>
2011-09-21 01:25:49 +0000

    Sarted on AI

--- trunk/ block-walker/block-walker/src/com/gooogle/code/blockWalker/AIupdate.java
+++ trunk/ block-walker/block-walker/src/com/gooogle/code/blockWalker/AIupdate.java
@@ -1,11 +1,14 @@
 package com.gooogle.code.blockWalker;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
 
 import org.anddev.andengine.engine.handler.timer.ITimerCallback;
 import org.anddev.andengine.engine.handler.timer.TimerHandler;
+import org.anddev.andengine.entity.IEntity;
+
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 
@@ -15,8 +18,9 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 public class AIupdate implements ITimerCallback {
 	
 	Random rand = new Random(System.nanoTime());
-	private static LinkedList<Monster> monsters = new LinkedList<Monster>();
 	private static LinkedList<Body> platforms = new LinkedList<Body>();
+	private static HashMap<IEntity, Monster> brain = new HashMap<IEntity, Monster>();
+	
 	
 	AIupdate() {
 		Iterator<Body> temp1 = Resources.getmPhysicsWorld().getBodies();
@@ -31,20 +35,12 @@ public class AIupdate implements ITimerCallback {
 	@Override
 	public void onTimePassed(TimerHandler pTimerHandler) {
 		pTimerHandler.reset();
-		for (Monster temp : monsters) {
-			switch (rand.nextInt(3)) {
-				case 0:
-					temp.right();
-					break;
-				case 1:
-					temp.left();
-					break;
-			}
-		}
-		
+		//update
 	}
 	
 	static void addMonster(Monster mon) {
-		monsters.add(mon);
+		brain.put(null, mon);
 	}
+	
+	
 }
